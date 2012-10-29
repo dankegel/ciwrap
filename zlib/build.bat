f3644a3044ce838423024b2716a1d749b9072ea7
Dan Kegel <dank@kegel.com>
2012-10-28 17:39:31 -0700

    Remove junk files

--- zlib/build.bat
+++
@@ -1,9 +0,0 @@
-mkdir msvcbuild
-cd msvcbuild
-cmake -G "Visual Studio 10" ..
-
-call settings.bat
-vcexpress zlib.sln /build "Release|Win32"
-
-vcexpress zlib.sln /build "Release|Win32" /project RUN_TESTS
-type msvcbuild\Testing\Temporary\LastTest.log
